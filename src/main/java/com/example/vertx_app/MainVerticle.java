aa34cc117cfe6ece20cbba945cf1adb80f438935
Ahmad Al Khateeb <al.khateeb.ahmad96@gmail.com>
2022-12-06 10:37:10 +0100

    works with the react app, initial update

--- src/main/java/com/example/vertx_app/MainVerticle.java
+++ src/main/java/com/example/vertx_app/MainVerticle.java
@@ -1,18 +1,22 @@
 package com.example.vertx_app;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.MultiMap;
-import io.vertx.core.Promise;
+import io.vertx.core.*;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.CorsHandler;
 import io.vertx.mysqlclient.MySQLConnectOptions;
 import io.vertx.mysqlclient.MySQLPool;
-import io.vertx.sqlclient.PoolOptions;
-import io.vertx.sqlclient.Row;
-import io.vertx.sqlclient.RowSet;
-import io.vertx.sqlclient.SqlClient;
+import io.vertx.sqlclient.*;
+
+import javax.swing.tree.RowMapper;
 
 public class MainVerticle extends AbstractVerticle {
   private static final String LIST_ALL_POSTS_ADDR ="vertx.list_all_posts";
@@ -23,38 +27,9 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start() throws Exception {
+    Vertx vertx = Vertx.vertx();
 
-    // Create a Router
-    Router router = Router.router(vertx);
-
-    // Mount the handler for all incoming requests at every path and HTTP method
-    router.route().handler(context -> {
-      // Get the address of the request
-      String address = context.request().connection().remoteAddress().toString();
-      // Get the query parameter "name"
-      MultiMap queryParams = context.queryParams();
-      String name = queryParams.contains("name") ? queryParams.get("name") : "unknown";
-      // Write a json response
-      context.json(
-        new JsonObject()
-          .put("name", name)
-          .put("address", address)
-          .put("message", "Hello " + name + " connected from " + address)
-      );
-    });
-
-    // Create the HTTP server
-    vertx.createHttpServer()
-      // Handle every request using the router
-      .requestHandler(router)
-      // Start listening
-      .listen(8888)
-      // Print the port
-      .onSuccess(server ->
-        System.out.println(
-          "HTTP server started on port " + server.actualPort()
-        )
-      );
+    HttpServer httpServer = vertx.createHttpServer();
 
     MySQLConnectOptions connectOptions = new MySQLConnectOptions()
       .setPort(3306)
@@ -63,35 +38,65 @@ public class MainVerticle extends AbstractVerticle {
       .setUser("root")
       .setPassword("rootroot");
 
-    // Pool options
+// Pool options
     PoolOptions poolOptions = new PoolOptions()
       .setMaxSize(5);
 
-    // Create the pooled client
+// Create the pooled client
     MySQLPool client = MySQLPool.pool(vertx, connectOptions, poolOptions);
 
-    client
-      .query("SELECT * FROM user WHERE username='ahmad'")
-      .execute(ar -> {
-        if (ar.succeeded()) {
-          RowSet<Row> result = ar.result();
-          System.out.println("Got " + result.size() + " rows ");
-          router.get("/:db").handler(re -> re.response().end(String.valueOf(result.size())));
-          router.route().handler(context ->
-            context.json(new JsonObject().put("the size", result.size()))
-            );
-          vertx.createHttpServer().requestHandler(router).listen(8888)
-            .onSuccess(server ->
-              System.out.println(
-                "The new db endpoint " + "db"
-              ));
-          for (Row row : result) {
-
-            System.out.println("User " + row.getString(0) + " " + row.getString(1) + " " + row.getString(2) + " " + row.getInteger(3));
-          }
-        } else {
-          System.out.println("Failure: " + ar.cause().getMessage());
-        }
+    Router router = Router.router(vertx);
+    Route handler1 = router
+      .get("/hello/:name")
+      .handler(CorsHandler.create("*")
+        .allowedMethod(HttpMethod.GET)
+        .allowedMethod(HttpMethod.OPTIONS)
+        .allowCredentials(true)
+        .allowedHeader("Access-Control-Allow-Method")
+        .allowedHeader("Access-Control-Allow-Origin")
+        .allowedHeader("Access-Control-Allow-Credentials")
+        .allowedHeader("Access-Control-Allow-Headers")
+        .allowedHeader("Content-Type"))
+      .handler(routingContext -> {
+        client
+          .query("SELECT * FROM user")
+          .execute(ar -> {
+            if (ar.succeeded()) {
+              RowSet<Row> result = ar.result();
+              System.out.println("Got " + result.size() + " rows ");
+              for (Row row : result) {
+                System.out.println("User " + row.getString(0) + " " + row.getString(1) + " " + row.getString(2) + " " + row.getInteger(3));
+              }
+              HttpServerResponse response = routingContext.response();
+              response.setChunked(true);
+              //response.write("Number of users " + result.size() + "\n");
+              //for (Row row : result){
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.put("amount_users", result.size());//.getValue("username")
+                System.out.println("The amount of users::: " + jsonObject.getInteger("amount_users"));
+                response//.setStatusCode(200).setStatusMessage("OK")
+                  .putHeader("content-type", "application/json")
+                  .putHeader("Access-Control-Allow-Origin", "*")
+                  .putHeader("Access-Control-Allow-Methods", "GET")
+                  .end(jsonObject.getString("amount_users"));//.encode()
+                  //.write(jsonObject.encode());
+              //}
+              /*
+              response//.setStatusCode(200).setStatusMessage("OK")
+                .putHeader("content-type", "application/json")
+                .putHeader("Access-Control-Allow-Origin", "*")
+                .putHeader("Access-Control-Allow-Methods", "GET")
+                .end();
+               */
+            } else {
+              System.out.println("Failure: " + ar.cause().getMessage());
+            }
+          });
       });
+
+
+    httpServer
+      .requestHandler(router)
+      .listen(8080);
   }
 }
